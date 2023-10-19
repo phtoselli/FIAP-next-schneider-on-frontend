4fdfeddea6d228ef84fa18bf2225b0804eff6c7a
phtoselli <phtoselli@gmail.com>
2023-10-19 11:12:15 -0300

    Add: create project base + header

---
+++ src/app/page.jsx
@@ -0,0 +1,28 @@
+import Header from '@/components/Header';
+import Image from 'next/image';
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+
+      <section id='hero' className='container-fluid'>
+        <div className='container'>
+
+        </div>
+      </section>
+
+      <section id='pitch' className='container-fluid'>
+        <div className='container'>
+
+        </div>
+      </section>
+
+      <section id='about' className='container-fluid'>
+        <div className='container'>
+
+        </div>
+      </section>
+    </main>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import logo from '../images/logo.png';
+
+export default function Header() {
+  return (
+    <header className='container-fluid mb-5 bg-white'>
+      <div className='container'>
+        <div className='row'>
+
+          <div className='col-12 d-flex align-items-center justify-content-center mt-3'>
+            <a href="/">
+              <Image src={logo} width={40}/>
+            </a>
+          </div>
+
+          <div className='col-12 d-flex align-items-center justify-content-center mt-2'>
+            <nav className='nav justify-content-end'>
+              <a className='nav-link link-success' href="/">Início</a>
+              <a className='nav-link link-success' href="#pitch">Vídeo pitch</a>
+              <a className='nav-link link-success' href="#about">Sobre o projeto</a>
+              <a className='nav-link link-success' href="/home">Entrar</a>
+            </nav>
+          </div>
+
+        </div>
+      </div>
+      <hr className='mt-4 mb-4' style={{ opacity: '1%'}}/>
+    </header>
+  )
+}
