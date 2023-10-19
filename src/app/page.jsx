3107921409e9ab5384f00e2e0560da59699e5b0c
phtoselli <phtoselli@gmail.com>
2023-10-19 11:51:22 -0300

    Add: create hero in App page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,17 +2,35 @@ import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
 import Image from 'next/image';
+import schneider from '@/images/schneider.png';
 
 export default function App() {
   return (
     <main>
       <Header />
 
-      <section id='hero' className='container-fluid'>
+      <section id='hero' className='pt-5 pb-5 container-fluid' style={{ backgroundImage: `url('/hero.jpg')`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
         <div className='container'>
+
+          <div className='row'>
+            <div className='col-12 col-md-6'>
+              <p>.</p>
+            </div>
+
+            <div className='col-12 col-md-6'>
+              <div className='d-flex justify-content-between flex-column' style={{ maxWidth: '400px' }}>
+                <h1 className='text-white' style={{ textShadow: '1px 1px 8px black' }}>Colaborador Schneider, seja bem-vindo ao ON!</h1>
+                <p className='text-white' style={{ opacity: '70%' }}>Faça seu cadastro e junte-se à uma rede de pessoas unidas no propósitos de fazer das suas ações, um passo para o desenvolvimento sustentável.</p>
+                <Image src={schneider} width={200}/>
+                <a className='btn btn-success' href="/">Entre agora</a>
+              </div>
+            </div>
+          </div>
 
         </div>
       </section>
+
+      <hr className='mt-5 mb-5' style={{ opacity: '1%'}}/>
 
       <section id='pitch' className='container-fluid'>
         <div className='container'>
@@ -20,11 +38,23 @@ export default function App() {
         </div>
       </section>
 
+      <hr className='mt-5 mb-5' style={{ opacity: '1%'}}/>
+
       <section id='about' className='container-fluid'>
         <div className='container'>
 
         </div>
       </section>
+
+      <hr className='mt-5 mb-5' style={{ opacity: '1%'}}/>
+
+      <section id='painel' className='container-fluid'>
+        <div className='container'>
+
+        </div>
+      </section>
+
+      <hr className='mt-5 mb-5' style={{ opacity: '1%'}}/>
 
       <Footer />
     </main>
