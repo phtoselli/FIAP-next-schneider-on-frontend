00ea4909146d5b1e49898e2cef8e87de959f93e9
phtoselli <phtoselli@gmail.com>
2023-10-26 09:00:42 -0300

    Add: create a .json to learn section and dinamic cards and pages

--- src/pages/learn.jsx
+++ src/pages/learn.jsx
@@ -1,10 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import { useEffect, useState } from 'react';
+
+import Error from '@/components/Error';
 import Footer from '@/components/Footer';
 import Header from '@/components/HeaderApp';
 
+import json from '@/database/learnArray.json';
+
+
 export default function Learn() {
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if(!json){
+      setData(null);
+    }
+
+    setData(json);
+  });
+
+  function cardsGenerate(param) {
+    if (!data) {
+      return (
+        <Error error={'Ops!'} message={'Nenhuma matéria foi encontrada.'} />
+      )
+    }
+
+    return param.map((obj, key) => {
+      return (
+        <div className="col p-4" key={key}>
+            <div className="card p-4">
+              <h4 className="card-title">{obj.title}</h4>
+              <p className="text-secondary" style={{ minWidth: '150px' }}>
+                {obj.description}
+              </p>
+              <a className='btn btn-success' href={`/learn/${obj.id}`}>Acessar</a>
+            </div>
+          </div>
+      )
+    });
+  }
 
   return (
     <main>
@@ -13,118 +51,12 @@ export default function Learn() {
       <section className='container'>
 
         <div className="row">
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Energias Renováveis</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Aprenda sobre fontes de energia limpa como solar, eólica e hidrelétrica para um futuro mais verde.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Reciclagem e Gerenciamento de Resíduos</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Descubra práticas de reciclagem eficazes e como gerenciar resíduos de forma sustentável.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Agricultura Sustentável</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Explore técnicas agrícolas que preservam o solo, economizam água e promovem a segurança alimentar.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Conservação da Biodiversidade</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Saiba como proteger a fauna e a flora, preservando ecossistemas preciosos em todo o mundo.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Mobilidade Sustentável</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Estude alternativas de transporte ecoamigáveis, como veículos elétricos e transporte público eficiente.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Design Ecológico</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Explore princípios de design que minimizam o impacto ambiental na arquitetura e produtos.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Ética Ambiental</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Aprofunde-se nos dilemas éticos relacionados à sustentabilidade e tome decisões informadas.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Educação Ambiental</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Descubra como educar e inspirar outros a se tornarem defensores do meio ambiente.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Políticas de Sustentabilidade</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Analise políticas governamentais e internacionais que moldam práticas sustentáveis e regulamentações.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Economia Verde</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Explore modelos econômicos que equilibram crescimento e preservação do meio ambiente.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Sustentabilidade na Indústria</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Saiba como empresas podem adotar práticas sustentáveis para reduzir o impacto ambiental.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
-          <div className="col p-4">
-            <div className="card p-4">
-              <h4 className="card-title">Mudanças Climáticas</h4>
-              <p className="text-secondary" style={{ minWidth: '150px' }}>
-                Compreenda as causas e consequências das mudanças climáticas, bem como estratégias de mitigação.
-              </p>
-              <a className='btn btn-success' href="/learn">Acessar</a>
-            </div>
-          </div>
+
+        { data ? cardsGenerate(data) : <Error error={'Ops!'} message={'Nenhuma matéria foi encontrada.'} />}
+
         </div>
 
       </section>
-
       <Footer />
     </main>
   )
---
+++ src/pages/learn/[learn].jsx
@@ -0,0 +1,83 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import json from '@/database/learn.json';
+
+import Error from '@/components/Error';
+import Footer from '@/components/Footer';
+import Header from '@/components/HeaderApp';
+import Loading from '@/components/Loading';
+
+export default function Learn() {
+  const router = useRouter();
+
+  const [url, setUrl] = useState();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if(!json){
+      setData(null);
+    }
+
+    setData(json);
+  });
+
+  useEffect(() => {
+    setUrl(router.query.learn);
+  }, [router.query.learn]);
+
+  function learnGenerate(param){
+    if (!data[param] || !data) {
+      return (
+        <Error error={404} message={'Essa página não existe.'} />
+      )
+    }
+
+    return (
+      <div className='container'>
+        <article className='container'>
+          <h2>{data[param].title}</h2>
+          <h6 className='text-secondary'>{data[param].subtitle}</h6>
+
+          <hr className='mt-4 mb-4' style={{ opacity: '0%' }}/>
+
+          <div className='row'>
+            {
+              data[param].content.map((content, key) => {
+                return (
+                  <div className='container mt-2 mb-2' key={key}>
+                    <p className='text-secondary'><span className='fw-semibold'>{content.emphasis}</span> {content.paragraph}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+
+        </article>
+      </div>
+    )
+  };
+
+  return (
+    <main>
+      <Header />
+
+      <div className='container'>
+        <div className='container'>
+          <a href="/learn">
+            <i className='btn btn-outline-success rounded-circle bi bi-chevron-left fs-4 fw-bold'></i>
+          </a>
+        </div>
+        <hr className='mt-3 mb-3' style={{ opacity: '0%' }} />
+      </div>
+
+      { url ? learnGenerate(url) : <Loading />}
+
+      <hr className='mt-5 mb-5' style={{ opacity: '0%' }} />
+      <Footer />
+    </main>
+  )
+}
